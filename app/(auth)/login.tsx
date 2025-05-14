a26ba9915446e9454943d445c702c400a3a649dc
Nguyen Trung Kien <nguyentrungkien17102004@gmail.com>
2025-05-14 22:17:57 +0700

    fea: add loagin and welcome page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,15 +1,93 @@
+import BackButton from "@/components/BackButton";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
 import ScreenWrapper from "@/components/ScreenWrapper";
-import React from "react";
-import { StyleSheet, Text } from "react-native";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import React, { useRef, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleSubmit = async () => {};
+
   return (
     <ScreenWrapper>
-      <Text>Login</Text>
+      <View style={styles.container}>
+        <BackButton iconSize={28} />
+
+        <View style={{ gap: 5, marginTop: spacingY._20 }}>
+          <Typo size={30} fontWeight={"800"}>
+            Hey,
+          </Typo>
+          <Typo size={30} fontWeight={"800"}>
+            Welcome Back
+          </Typo>
+        </View>
+
+        <View style={styles.form}>
+          <Typo size={16} color={colors.textLighter}>
+            Login now to track all your expenses
+          </Typo>
+
+          <Input
+            placeholder="Enter your email"
+            onChangeText={(value) => (emailRef.current = value)}
+          />
+          <Input
+            placeholder="Enter your password"
+            onChangeText={(value) => (passwordRef.current = value)}
+          />
+
+          <Typo size={14} color={colors.text} style={{ alignSelf: "flex-end" }}>
+            Forgot Password?
+          </Typo>
+
+          <Button onPress={handleSubmit}>
+            <Typo size={21} fontWeight={"700"} color={colors.black}>
+              Login
+            </Typo>
+          </Button>
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: spacingY._30,
+    paddingHorizontal: spacingX._20,
+  },
+  welcomeText: {
+    fontSize: verticalScale(240),
+    fontWeight: "bold",
+    color: colors.text,
+  },
+  form: {
+    gap: spacingY._20,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: "500",
+    color: colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  footerText: {
+    textAlign: "center",
+    color: colors.text,
+    fontSize: verticalScale(15),
+  },
+});
