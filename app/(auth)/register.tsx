099bcddf893a8d1719892c944ca5e0baf1687cc6
Nguyen Trung Kien <nguyentrungkien17102004@gmail.com>
2025-05-14 21:16:01 +0700

    first commit

---
+++ app/(auth)/register.tsx
@@ -0,0 +1,15 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+const Register = () => {
+  return (
+    <ScreenWrapper>
+      <Text>Register</Text>
+    </ScreenWrapper>
+  );
+};
+
+export default Register;
+
+const styles = StyleSheet.create({});
---
+++ index.ts
@@ -0,0 +1,2 @@
+// Otherwise import your CSS file in the index.js file:
+import "./global.css";
